546efddd26f5fbd633eb9d567bdf8b9a9102ea99
Dominic Brown <domingomonk@gmail.com>
2021-06-12 17:48:19 -0400

    Blood splatter effect when passives become zombies

--- scripts/Characters/States/Passive.gd
+++ scripts/Characters/States/Passive.gd
@@ -1,6 +1,7 @@
 extends State
 
 onready var host = get_parent().get_parent()
+onready var blood_splash = preload("res://scenes/BloodSplash.tscn")
 
 export var start_skin : Material
 export var zombie_skin : Material
@@ -11,17 +12,16 @@ var current_health : int = max_health
 var current_damage_per_frame = 0
 
 signal change_state
+signal infected(blood_splash, transform)
 
 func enter():
 	change_skin(start_skin)
 
 func exit():
-	pass
+	emit_signal("infected", blood_splash, host.global_transform)
 
 func _ready():
-	print([start_skin.albedo_color.r, start_skin.albedo_color.g, start_skin.albedo_color.b])
-	print([zombie_skin.albedo_color.r, zombie_skin.albedo_color.g, zombie_skin.albedo_color.b])
-	print(host.get_node("Skin").get_mesh().surface_get_material(0))
+	pass
 	
 
 func _process(delta):
@@ -34,8 +34,6 @@ func _process(delta):
 func interpolate_skin_color():
 	var current_skin = host.get_node("Skin").get_mesh().surface_get_material(0).duplicate()
 	var percent_health = float(current_health) / max_health
-	
-	print([zombie_skin.albedo_color.r, current_skin.albedo_color.r, percent_health])
 	
 	var new_color = lerp(zombie_skin.albedo_color, start_skin.albedo_color, percent_health)
 
@@ -55,6 +53,7 @@ func get_damage_if_zombie(area):
 
 func change_skin(material):
 	host.get_node("Skin").get_mesh().surface_set_material(0, material)
+	
 
 func _on_InfectionRadius_area_entered(area):
 	current_damage_per_frame += get_damage_if_zombie(area)
