b78a09f309be84a02388ac13b35b998e56b5c033
Dominic Brown <domingomonk@gmail.com>
2021-06-13 00:52:25 -0400

    Added zombie tracking

--- scripts/GameController.gd
+++ scripts/GameController.gd
@@ -1,36 +1,38 @@
 extends Spatial
 
-#onready var remaining_humans_label = $RemainingText
-
 var zombies = []
 var humans = []
 var zombie_count = 0
 var human_count = 0
 
 func _ready():
-	get_humans()
+	count_zombies()
+	count_humans()
 	set_label_text()
 	
-func get_zombies():
+func count_zombies():
 	for child in get_children():
 		if child.has_method("get_state") and child.get_state().get_name() == "Zombie":
-			zombies.insert[child]
+			zombies.append(child)
 			zombie_count += 1
-			
-func get_humans():
+
+func count_humans():
 	for child in get_children():
 		if child.has_method("get_state") and (child.get_state().get_name() == "Passive" \
 			or child.get_state().get_name() == "Aggressive"):
 			humans.append(child)
-			child.get_state().connect("change_state", self, "human_infected")
+			child.get_state().connect("change_state", self, "human_infected", [child])
 			human_count += 1
 
-func human_infected(_state):
-	print("got infected signal")
+func get_zombies():
+	return zombies
+
+func human_infected(new_state, node):
 	human_count -= 1
+	zombies.append(node)
 	set_label_text()
 
 func set_label_text():
-	if $RemainingText:
-		$RemainingText.text = "- Humans Left to Meet: " + str(human_count)
+	if $LevelText/RemainingText:
+		$LevelText/RemainingText.text = "- Humans Left to Meet: " + str(human_count)
 	
--- scripts/Rifle.gd
+++ scripts/Rifle.gd
@@ -12,7 +12,7 @@ func enter(target):
 func spawn_bullet():
 	if target:
 		var fired_bullet = bullet.instance()
-		get_tree().get_root().get_node("World").add_child(fired_bullet)
+		get_tree().get_root().get_child(0).add_child(fired_bullet)
 		fired_bullet.global_transform.origin = $Flash.global_transform.origin
 		fired_bullet.look_at(target.global_transform.origin, Vector3.UP)
 		fired_bullet.enter()
